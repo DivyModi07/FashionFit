14e465667cae08e83e1fbd9bfc1e4ee0aa318219
DivyModi12 <divymodi1235@gmail.com>
2025-07-05 17:19:54 +0530

    new - Folder Structure + DB

--- backend/fashion_backend/fashion_backend/settings.py
+++ backend/fashion_backend/fashion_backend/settings.py
@@ -84,12 +84,24 @@ WSGI_APPLICATION = 'fashion_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'FashionFit_db',
+        'USER': 'postgres',
+        'PASSWORD': 'DivyModi_12',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- backend/fashion_backend/users/models.py
+++ backend/fashion_backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    sid = models.AutoField(primary_key=True)
+    sname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.sid} - {self.sname}"
